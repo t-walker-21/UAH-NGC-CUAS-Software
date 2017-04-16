18468f3c62a9c5eae0d9c971cc403effdc822589
Tevon Walker <tjw0018@uah.edu>
2017-04-16 16:52:06 -0500

    modified scan script to use ALFA wifi adapter instead of integrated wifi

--- scan.sh
+++ scan.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 	rm -f dump*
-	ifconfig wlan0 down
-	xterm -hold -e airodump-ng -w dump wlan0 & tput setaf 3; echo "Searching for APs...$(tput sgr 0)"	# Scan for APs
+	ifconfig wlan1 down
+	xterm -hold -e airodump-ng -w dump wlan1 & tput setaf 3; echo "Searching for APs...$(tput sgr 0)"	# Scan for APs
